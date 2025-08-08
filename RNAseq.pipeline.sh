c0ee6b251286b50151e24de1de98675115dd4f22
Camous Moslemi <38889158+kamiboy@users.noreply.github.com>
2025-08-08 12:05:31 +0200

    Update RNAseq.pipeline.sh

--- RNAseq.pipeline.sh
+++ RNAseq.pipeline.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-/Volumes/N1/RNAseq/cufflinks/gffread /Volumes/N1/INPUT/GENOME/BD/REF/annotation/BdistachyonBd21_3_537_v1.2.gene.gff3 -T -o /Volumes/N1/RNAseq/01_output/BD.gtf
-python3 /Volumes/N1/RNAseq/hisat2/hisat2_extract_splice_sites.py /Volumes/N1/RNAseq/01_output/BD.gtf > /Volumes/N1/RNAseq/01_output/BD.ss
-python3 /Volumes/N1/RNAseq/hisat2/hisat2_extract_exons.py /Volumes/N1/RNAseq/01_output/BD.gtf > /Volumes/N1/RNAseq/01_output/BD.exon
-/Volumes/N1/RNAseq/hisat2/hisat2-build -p 8 --exon /Volumes/N1/RNAseq/01_output/BD.exon --ss /Volumes/N1/RNAseq/01_output/BD.ss /Volumes/N1/INPUT/GENOME/BD/REF/assembly/BdistachyonBd21_3_537_v1.0.fa /Volumes/N1/RNAseq/01_output/BD
-for i in 201 202 203 204 205 206 207 208 210 219 220 221 222 223 225 226 227 228 229 230 231 232 233 234 235 237 238 239 240 241 242 243 244 245 246 247 248 249 250 251 252 253 254 255 256 257 258 259 260 261 262 263 264 265 266 267 268 269 270 271 272 273 274 275 276 277 279 280 282 283 284 285 286 287 288 289 290 291 292 293 294 296 297 298 299 300 301 302 303 304 306 307 308 309 310 311 312 313 314 315 316 317 318 319 320 321 322 323 324 325 326 327 328 329 330 331 332 333 334 335 336 337 338 339 340 341 342 343 344 345 346 347 348 349 350 351 352 353 354 355 356 357 358 359 360 361 362 363 364 365 366 367 368 369 370 371 372 373 374 376 377 378 380 381 382 383 384 385 386 387 388 389 390 391 392 393 394 395 396 397 399 400 401 402 403 404 405 406 407 408 409 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 431 434 435 437 438 439 440 441 442 443 444 445 446 447 448 449 450 451 452 453 454 455 456 457 458 459 460 461 462 463 464 465 466 467 468 469 470 471 472 473 475 476 477 478 480 481 482 483 485 489 490 491 492 493 494 495 496 497 498 499 500 501 502 503 504 505 506 507 508 509 510 511 512 513 514 515 516 517 518 519 520 521 522 523 524 525 526 527 528 529 530 531 532 533 534 535 536 537 538 539 540 541 542 543 544 545 546 547 548 549 550 551 552 554 555 556 557 558 559 560 561 562 563 564 565 566 567 568 569 570 571 572 573 574 576 577 578 580 581 582 583 585 586 587 588 589 590 591 592 593 594 595 596 597 598 599 600
+#List of lines to process
+LINES='100 130 156 181 26 53 802 827 852 102 131 157 182 27 54 803 828 853 103 132 158 183 28 55 804 829 854 104 133 159 185 2 56 805 82 855 105 134 15 186 30 57 806 830 856 106 135 160 187 31 58 807 831 857 107 136 162 188 32 59 808 832 85 108 137 163 189 33 5 809 833 86 109 138 164 18 34 60 80 834 87 10 139 165 190 35 61 810 835 88 110 13 166 191 37 62 811 836 89 111 140 167 192 38 63 812 837 8 112 141 168 193 39 64 813 839 90 113 142 169 194 3 65 814 83 91 114 143 16 196 40 67 815 840 92 117 144 170 197 41 69 816 841 94 118 145 171 198 42 6 817 842 95 119 146 172 199 44 70 818 843 96 11 147 173 19 45 71 819 844 97 121 148 174 1 46 72 81 845 98 122 149 175 200 47 73 820 846 99 123 150 176 20 48 74 821 847 9 124 151 177 21 49 76 822 848 126 152 178 22 4 77 823 849 128 153 179 23 50 79 824 84 129 154 17 24 51 7 825 850 12 155 180 25 52 801 826 851'
+#The folder containing the tool binaries used for processing
+TFOLDER=/home/camous/sieve/Camous/TOOLS
+#The input folder that contains 00_fastq, where the RNAseqs are read from
+IFOLDER=/home/camous/sieve/90-1080311147/00_fastq
+#The putput folder that contains 01_output into which files are written
+OFOLDER=/home/camous/sieve/Camous/90-1080311147/01_output
+
+$TFOLDER/cufflinks/gffread /Volumes/N1/INPUT/GENOME/BD/REF/annotation/BdistachyonBd21_3_537_v1.2.gene.gff3 -T -o $OFOLDER/BD.gtf
+python3 $TFOLDER/hisat2/hisat2_extract_splice_sites.py $OFOLDER/BD.gtf > $OFOLDER/BD.ss
+python3 $TFOLDER/hisat2/hisat2_extract_exons.py $OFOLDER/BD.gtf > $OFOLDER/BD.exon
+$TFOLDER/hisat2/hisat2-build -p 8 --exon $OFOLDER/BD.exon --ss $OFOLDER/BD.ss /Volumes/N1/INPUT/GENOME/BD/REF/assembly/BdistachyonBd21_3_537_v1.0.fa $OFOLDER/BD
+for i in $LINES
 do
-	fastp -w 8 -i '/Volumes/N1/RNAseq/00_fastq/'$i'A1_R1_001.fastq.gz' -I '/Volumes/N1/RNAseq/00_fastq/'$i'A1_R2_001.fastq.gz' -o '/Volumes/N1/RNAseq/01_output/'$i'_R1.fq.gz' -O '/Volumes/N1/RNAseq/01_output/'$i'_R2.fq.gz'  -j '/Volumes/N1/RNAseq/01_output/'$i'_fastp.json' -h '/Volumes/N1/RNAseq/01_output/'$i'_fastp.html'
-    /Volumes/N1/RNAseq/hisat2/hisat2 -p 8 -x /Volumes/N1/RNAseq/01_output/BD -1 '/Volumes/N1/RNAseq/01_output/'$i'_R1.fq.gz' -2 '/Volumes/N1/RNAseq/01_output/'$i'_R2.fq.gz' | samtools view -bSh > '/Volumes/N1/RNAseq/01_output/'$i'_aligned.bam'
-	/Volumes/N1/RNAseq/subread/featureCounts -T 8 -p -a '/Volumes/N1/RNAseq/01_output/BD.gtf' -o '/Volumes/N1/RNAseq/01_output/'$i'_aligned.FC' '/Volumes/N1/RNAseq/01_output/'$i'_aligned.bam'
+	fastp -w 8 -i $IFOLDER'/'$i'A1_R1_001.fastq.gz' -I $IFOLDER'/'$i'A1_R2_001.fastq.gz' -o $OFOLDER'/'$i'_R1.fq.gz' -O $OFOLDER'/'$i'_R2.fq.gz'  -j $OFOLDER'/'$i'_fastp.json' -h $OFOLDER'/'$i'_fastp.html'
+    $TFOLDER/hisat2/hisat2 -p 8 -x $OFOLDER/BD -1 $OFOLDER'/'$i'_R1.fq.gz' -2 $OFOLDER'/'$i'_R2.fq.gz' | samtools view -bSh > $OFOLDER'/'$i'_aligned.bam'
+	$TFOLDER/subread/featureCounts -T 8 -p -a $OFOLDER'/BD.gtf' -o $OFOLDER'/'$i'_aligned.FC' $OFOLDER'/'$i'_aligned.bam'
 done
