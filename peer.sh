7fa5cb94c9660b7cd18987ea13f6da35db8e1790
Camous Moslemi <38889158+kamiboy@users.noreply.github.com>
2025-11-17 14:00:50 +0100

    Add files via upload

--- peer.sh
+++ peer.sh
@@ -1,4 +1,4 @@
-cd /media/shd-sieve/N1/PEER
+cd /Volumes/N1/Embeddings/DATA/PEER
 for n in 1 2 3 4 6 7 8 9 11 12 13 14 15; do
-    ./bin/peertool -f peer.expression.csv -n $n -o peer_n-$n -i 1000
+    ./bin/peertool -f ../peer.expression.csv -n $n -o peer_n-$n -i 1000
 done
